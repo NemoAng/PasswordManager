0acbd3cc5f1bdf3b1824c3bacd887dc4de8083ec
Nemo <nemo.xun.jin.wang@gmail.com>
2021-11-15 11:34:39 -0600

    first commit

---
+++ app/src/main/java/com/nemowang/passwordmanager/ui/passgen/PassgenViewModel.java
@@ -0,0 +1,19 @@
+package com.nemowang.passwordmanager.ui.passgen;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
